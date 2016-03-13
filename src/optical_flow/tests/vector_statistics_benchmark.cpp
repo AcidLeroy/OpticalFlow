bf751a46aa3942aabf80e3db1505ecb07b06b4da
Cody W. Eilar <Cody.Eilar@Gmail.com>
2016-03-13 12:00:07 -0600

    Lk flow is now a template only library
    Signed-off-by: Cody W. Eilar <Cody.Eilar@Gmail.com>

--- src/optical_flow/tests/vector_statistics_benchmark.cpp
+++ src/optical_flow/tests/vector_statistics_benchmark.cpp
@@ -15,15 +15,15 @@ CELERO_MAIN
 
 namespace oflow {
 
-vector_type baseline_vector{std::vector<cv::Point2f>(8),
-                            std::vector<cv::Point2f>(8)};
+vector_type baseline_vector{
+    {std::vector<cv::Point2f>(8), std::vector<cv::Point2f>(8)}};
 
 BASELINE(VectorStatistics, Constructor, 100, 1e5) {
   celero::DoNotOptimizeAway(VectorStatistics<>(baseline_vector));
 }
 
-vector_type big_vector{std::vector<cv::Point2f>(1024),
-                       std::vector<cv::Point2f>(1024)};
+vector_type big_vector{
+    {std::vector<cv::Point2f>(1024), std::vector<cv::Point2f>(1024)}};
 BENCHMARK(VectorStatistics, BigConstructor, 100, 1e5) {
   celero::DoNotOptimizeAway(VectorStatistics<>(big_vector));
 }
