74c180ef8fd68f3617fbd75a8d734092385d5fa4
Cody W. Eilar <Cody.Eilar@Gmail.com>
2016-07-05 07:56:20 -0600

    Add ability to write output to s3
    Signed-off-by: Cody W. Eilar <Cody.Eilar@Gmail.com>

--- src/aws_scripts/process_video_list.py
+++ src/aws_scripts/process_video_list.py
@@ -4,6 +4,7 @@
 from __future__ import print_function
 import argparse
 import boto3
+import uuid
 
 def main():
     parser = argparse.ArgumentParser(description="A utility to process videos on the SQS queue. The idea with "
@@ -22,15 +23,34 @@ def main():
     print("Using queue name: ", queue_name)
 
 
+
     # Get the SQS messages
     sqs = boto3.resource('sqs')
     queue = sqs.get_queue_by_name(QueueName=queue_name)
+
+    # Top level bucket directory to use for s3
+    default_output_dir="featureoutput"
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket(default_output_dir)
+    s3_client = boto3.client('s3')
+
 
     while(True):
         for message in queue.receive_messages(MessageAttributeNames=['Classification','S3Output']):
             classification = message.message_attributes.get('Classification').get('StringValue')
             s3_output = message.message_attributes.get('S3Output').get('StringValue')
-            print("Path = ", message.body, " with classification = ", classification, " Goes to path: ", s3_output)
+            output_string = "Path = "+ message.body+ " with classification = "+ classification+ " Goes to path: "+ \
+                            s3_output + '\n'
+            print(output_string)
+
+            #Write the results to s3
+            filename = str(uuid.uuid1()) + '.csv'
+            with open(filename, 'w') as f:
+                f.write(output_string)
+
+            file_path = s3_output + '/' + filename
+            s3_client.upload_file(filename, default_output_dir, file_path)
+
             message.delete()
 
 
