fa7a584272f37c5d76d03f9bcfdd55c835a6c60c
Cody Wilson Eilar <cweilar@sandia.gov>
2016-09-27 15:27:40 -0600

    Fixup dockerfile, write features to file
    Signed-off-by: Cody Wilson Eilar <cweilar@sandia.gov>

--- src/matlab/extract_features.m
+++ src/matlab/extract_features.m
@@ -1,7 +1,10 @@
-function extract_features( classification, video_file, method )
+function extract_features( classification, video_file, method, output_file )
 %EXTRACT_FEATURES Extract features from a video given a specific 
 % classification 
-%path_to_video = coder.newtype('constant', video_file);
+if ischar(classification) 
+    classification = str2num(classification); 
+end
+
 path_to_video = video_file;
 VisFlow   = StdMotionEst(path_to_video, method);
 [CenX_CDF, CenY_CDF, Orient_CDF, Histo_CDF, Motion_mag_CDF, Motion_orient_CDF] ...
@@ -17,7 +20,10 @@ FeatureVector = sprintf('%s,%s,%s,%s,%s,%s,%d\n', ...
    remove_brackets(mat2str(transpose(Motion_orient_CDF(:)))), ...
    classification); 
    
-fprintf(FeatureVector)
+fileID = fopen(output_file, 'w'); 
+fprintf(fileID, FeatureVector); 
+fprintf(FeatureVector);
+fclose(fileID); 
             
 end
 
